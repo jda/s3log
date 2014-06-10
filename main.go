78f01232129ec6dffc809e4080948b094b32aa56
Jon Auer <jda@coldshore.com>
2014-06-09 19:45:50 -0500

    document Entry

--- main.go
+++ main.go
@@ -9,24 +9,24 @@ import (
 
 // A Entry is a structured log entry that describes a S3 request
 type Entry struct {
-	Owner      string
-	Bucket     string
-	Time       time.Time
-	Remote     net.IP
-	Requester  string
-	RequestID  string
+	Owner      string    // user ID of bucket owner
+	Bucket     string    // bucket name
+	Time       time.Time // time when request was recieved
+	Remote     net.IP    // IP address of requester
+	Requester  string    // user ID of requester
+	RequestID  string    // request ID
 	Operation  string
-	Key        string
+	Key        string // key requested from bucket
 	RequestURI string
-	Status     int
-	Error      string
-	Bytes      int
-	Size       int
-	Total      int
-	Turnaround int
-	Referrer   string
-	UserAgent  string
-	Version    string
+	Status     int    // HTTP status code
+	Error      string // Error code
+	Bytes      int    // Bytes sent to requester
+	Size       int    // size of object requested
+	Total      int    // time spent serving request
+	Turnaround int    // time spent handling request before response is sent
+	Referrer   string // HTTP referred
+	UserAgent  string // HTTP UserAgent
+	Version    string // Request version ID
 }
 
 var logLine = regexp.MustCompile(`[^" ]+|("[^"]*")`)
